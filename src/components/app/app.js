022032fe737cf3a0c8d37cb842a9fd1a7e3fb872
koretskiyav <koretskiyav@gmail.com>
2022-01-21 14:19:01 +0200

    add button, header, normalize.css, styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,14 @@
 import { PureComponent } from 'react';
 import Restaurants from '../restaurants';
+import Header from '../header';
 
 export default class App extends PureComponent {
   render() {
     const { restaurants } = this.props;
+
     return (
       <div>
+        <Header />
         <Restaurants restaurants={restaurants} />
       </div>
     );
