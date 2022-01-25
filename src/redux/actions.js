d849adab52803791f1fffa008b5ebbdd68101e63
koretskiyav <koretskiyav@gmail.com>
2022-01-25 21:04:26 +0200

    add product.id to order

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
 import { DECREMENT, INCREMENT } from './reducer/constants';
 
-export const decrement = () => ({ type: DECREMENT });
-export const increment = () => ({ type: INCREMENT });
+export const decrement = (id) => ({ type: DECREMENT, id });
+export const increment = (id) => ({ type: INCREMENT, id });
